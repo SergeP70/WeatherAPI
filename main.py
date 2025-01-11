5be11547c158fdd249acda357a4898a8208ab922
sergeP70 <serge@pille.be>
2025-01-11 17:23:19 +0100

    Reading csv file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Building a Weather API with Flask
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -9,10 +11,30 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def contact(station, date):
+    temp = 0
+    filename = f"data/TG_STAID{station.zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["    DATE"] = pd.to_datetime(df["    DATE"])
+    print(df.columns)
+    print(df["    DATE"])
+    temp = df.loc[df['    DATE'] == '1860-01-05 00:00:00']['   TG'].squeeze() / 10
+    print(temp)
+
+
     return {"Station: ": station,
             "Date: ": date,
             "Temperature:": 23}
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
 
+
+
+"""
+~$ python3.12 -m pip install jupyterlab
+goto downloads:
+Downloads$ jupyter-lab
+zsh: command not found: jupyter-lab
+Downloads$ python3.12 -m jupyterlab
+
+"""
