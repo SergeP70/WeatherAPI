17c4988e8a7228e05fe4088a068e273d50283280
sergeP70 <serge@pille.be>
2025-01-12 09:56:02 +0100

    returning the temperature

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template
 import pandas as pd
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,19 +10,16 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def contact(station, date):
-    temp = 0
+    # station="10"
     filename = f"data/TG_STAID{station.zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    df["    DATE"] = pd.to_datetime(df["    DATE"])
-    print(df.columns)
-    print(df["    DATE"])
-    temp = df.loc[df['    DATE'] == '1860-01-05 00:00:00']['   TG'].squeeze() / 10
-    print(temp)
-
+    temperature = df.loc[df['    DATE'] == "1860-01-05"]['   TG'].squeeze()/10
+    print(temperature)
+    print(type(temperature))
 
     return {"Station: ": station,
             "Date: ": date,
-            "Temperature:": 23}
+            "Temperature:": temperature}
 
 if __name__ == '__main__':
     app.run(debug=True)
